b952e1e1fc1ba6e8d8c3cda4858ba90a2124cbc9
ram-9547 <rakesh95476266@gmail.com>
2024-08-11 23:33:36 +0530

    yes

---
+++ src/app/about/page.jsx
@@ -0,0 +1,81 @@
+"use client";
+import React from "react";
+import {
+  Heading,
+  SimpleGrid,
+  Image,
+  Center,
+  Box,
+  Text,
+} from "@chakra-ui/react";
+import InterestsCard from "./InterestsCard";
+import { interestsData } from "../../utils/constants";
+import { RevealWrapper } from "next-reveal";
+
+const Page = () => {
+  return (
+    <>
+      <Center m={5}>
+        <Box width="80vw" height="100%">
+          <RevealWrapper className="load-hidden" id="about" delay={300}>
+            <Heading className="sub-heading" size="md" my={3}>
+              ABOUT
+            </Heading>
+            <SimpleGrid
+              columns={[1, null, 2]}
+              gap={6}
+              fontSize="1.1rem"
+              marginBottom="4rem"
+              templateColumns={[null, null, "30% 70%"]}
+            >
+              <Image
+                objectFit="shrink"
+                src="/images/ar-profile-transformed.png"
+                alt="profile-image"
+                width={["20rem", "30rem", "20rem"]}
+                height={["20rem", "30rem", "20rem"]}
+                marginBottom={["1rem", "1rem", null]}
+              />
+
+              <Box textAlign="justify">
+                <Heading size="xl" mb={3}>
+                  👋 Hey there
+                </Heading>
+                <br />
+                <Text>
+                  I&apos;m <span className="name">Tharun Thondapu</span>, a passionate
+                  software developer, Highly motivated and adaptable individual with a strong interest in the IT management field,
+                  seeking an entry-level position to learn and grow within a leading IT company. Possess
+                  excellent communication, leadership, and teamwork skills, eager to apply them to contribute
+                  to the success of the organization
+                </Text>
+                <br />
+              </Box>
+            </SimpleGrid>
+          </RevealWrapper>
+          <RevealWrapper className="load-hidden" id="interests" delay={600}>
+            <Heading className="sub-heading" size="md" my={3}>
+              INTERESTS
+            </Heading>
+            <SimpleGrid
+              spacing={4}
+              templateColumns="repeat(auto-fill, minmax(270px, 1fr))"
+            >
+              {interestsData.map((data, index) => {
+                return (
+                  <InterestsCard
+                    interest={data.interest}
+                    icon={data.icon}
+                    key={index}
+                  />
+                );
+              })}
+            </SimpleGrid>
+          </RevealWrapper>
+        </Box>
+      </Center>
+    </>
+  );
+};
+
+export default Page;
